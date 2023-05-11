1b835c78ce9c57d1f309e5314aae5d49570a4503
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-10 23:28:22 -0400

    Removido react-bootstrap e bootstrap

--- src/components/PromotionBanner/Style.ts
+++ src/components/PromotionBanner/Style.ts
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const BannerContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 80px 0;
+  /* display: flex; */
+  /* justify-content: center; */
+  /* margin: 80px 0; */
 `;
 
 export const Image = styled.img`
-  margin: 0;
-  max-width: 300px;
-  height: 50vh;
-  margin: 0 20px;
-  border: 1px solid #000;
+  /* margin: 0; */
+  /* max-width: 300px; */
+  /* height: 50vh; */
+  /* margin: 0 20px; */
+  /* border: 1px solid #000; */
 
-  &:nth-child(2) {
+  /* &:nth-child(2) {
     margin-top: -40px;
-  }
+  } */
 `;
