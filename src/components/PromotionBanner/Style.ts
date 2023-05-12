837aa1277dec37de47cb685af7c46c1557d3205c
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-11 20:12:13 -0400

    Feature: instalado React Slick

--- src/components/PromotionBanner/Style.ts
+++ src/components/PromotionBanner/Style.ts
@@ -1,10 +1,26 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const BannerContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 80px 0;
-`;
+// export const BannerContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   margin: 80px 0;
+// `;
+
+// export const Image = styled.img`
+//   margin: 0;
+//   max-width: 300px;
+//   height: 50vh;
+//   margin: 0 20px;
+//   border: 1px solid #000;
+
+//   &:nth-child(2) {
+//     margin-top: -40px;
+//   }
+// `;
+
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Image = styled.img`
   margin: 0;
--- src/components/PromotionBanner/index.tsx
+++ src/components/PromotionBanner/index.tsx
@@ -1,11 +1,35 @@
-import { BannerContainer, Image } from "./Style";
+// import { BannerContainer, Image } from "./Style";
+
+// export default function PromotionBanner() {
+//   return (
+//     <BannerContainer>
+//       <Image src="../../../assets/images/1.png" alt="Image 1" />
+//       <Image src="../../../assets/images/2.png" alt="Image 2" />
+//       <Image src="../../../assets/images/3.png" alt="Image 3" />
+//     </BannerContainer>
+//   );
+// }
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Image } from "./Style";
 
 export default function PromotionBanner() {
   return (
-    <BannerContainer>
+    <Slider
+      infinite={true}
+      slidesToShow={3}
+      slidesToScroll={1}
+      centerMode={true}
+      autoplay={true}
+      autoplaySpeed={3000}
+      pauseOnHover={true}
+      className="center"
+    >
       <Image src="../../../assets/images/1.png" alt="Image 1" />
       <Image src="../../../assets/images/2.png" alt="Image 2" />
       <Image src="../../../assets/images/3.png" alt="Image 3" />
-    </BannerContainer>
+    </Slider>
   );
 }
