99b050a53a82353ff0d618399705b83fe76ffdd0
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-15 22:07:32 -0400

    Feature: instalado axios e react-query

--- src/components/ProductSearch/index.tsx
+++ src/components/ProductSearch/index.tsx
@@ -1,4 +1,21 @@
 import { InputDefault } from "./Style";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+
+
+export type Comida = {
+    food: string;
+    category: string;
+    kcal: number;
+  };
+
+const pesquisarComida = async (query: string): Promise<Comida[]> => {
+    const response = await axios.get(
+      `https://foods-json-server.onrender.com/foods?q=${query}`
+    );
+    return response.data;
+  };
 
 export default function ProductSearch() {
     return(
