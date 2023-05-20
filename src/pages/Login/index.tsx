b54b8c6b3b1073a645123e59b27630ae326550ae
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-20 18:28:31 -0400

    Estilizaçào dos inputs e botao pag login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,24 +2,25 @@ import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 import Header from "../../components/Header";
 import { useState } from "react";
+import { Button, Form, Input, Main } from "./styles";
 
 export default function Login() {
   const [open, setOpen] = useState(false);
 
   return (
-    <main>
+    <Main>
       <Header open={open} setOpen={setOpen} />
 
-      <h1>Fazer login</h1>
-      <form action="">
-        <input name="email" type="text" placeholder="email" />
-        <input name="senha" type="text" placeholder="senha" />
-        <button>Fazer login</button>
+      <Form action="">
+        <Input name="email" type="text" placeholder="Email" />
+        <Input name="senha" type="text" placeholder="Senha" />
+        <Button>Login</Button>
         <span>
           Cliente novo? <Link to={"/cadastro"}>Comece aqui.</Link>
         </span>
-      </form>
+      </Form>
+
       <Footer />
-    </main>
+    </Main>
   );
 }
