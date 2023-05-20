2c55672d14d68f3b8c8f8b43eb7e6cbc3bd39ca7
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-19 20:27:22 -0400

    Feature: adicionado formulario de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 
 export default function Login() {
   return (
     <main>
-      <h1>Login</h1>
+      <form action="">
+        <input name="email" type="text" placeholder="email"/>
+        <input name="senha" type="text" placeholder="senha"/>
+        <button>Fazer login</button>
+        <span>Cliente novo? <Link to={"/"}>Comece aqui.</Link></span>
+      </form>
       <Footer />
     </main>
   );
