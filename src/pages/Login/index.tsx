1cbc8e100e7c8bc045252acbbceecbde1fa1c649
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-21 23:07:13 -0400

    Feature: adicionado leitura dos inputs no na pag login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,17 +3,35 @@ import { Footer } from "../../components/Footer";
 import Header from "../../components/Header";
 import { useState } from "react";
 import { Button, Form, Input, Main } from "./styles";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
   const [open, setOpen] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <Main>
       <Header open={open} setOpen={setOpen} />
 
-      <Form action="">
-        <Input name="email" type="text" placeholder="Email" />
-        <Input name="senha" type="text" placeholder="Senha" />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: true })}
+        />
+        {errors.email && <span>Este campo é obrigatorio</span>}
+        <Input
+          type="password"
+          placeholder="Senha"
+          {...register("senha", { required: true })}
+        />
+        {errors.senha && <span>Este campo é obrigatorio</span>}
         <Button>Login</Button>
         <span>
           Cliente novo? <Link to={"/cadastro"}>Comece aqui.</Link>
