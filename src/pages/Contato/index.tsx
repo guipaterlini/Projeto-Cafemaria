6289fbb371d6c74c575c7ebbaa9a506292269613
Alexandre de Paula <alexandreaugusto@gmail.com>
2023-05-24 10:09:12 -0300

    FEAT-adicionada pagina de contatos com a rota

---
+++ src/pages/Contato/index.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "../../components/Footer";
+import "style.css"
+
+export default function Contato() {
+  return (
+    <main>
+      <h1>Contato</h1>
+      <Footer />
+    </main>
+  );
+}
