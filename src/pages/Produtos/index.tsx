45a1f9c2bb5a922777b4df616eced908b76cfea3
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-05-29 18:47:58 -0400

    Feature pagina produtos está dinamica

--- src/pages/Produtos/index.tsx
+++ src/pages/Produtos/index.tsx
@@ -1,214 +1,26 @@
 import { Footer } from "../../components/Footer";
 import Header from "../../components/Header";
-import React from "react";
-import ProductList, { Product } from "../../components/ProductList";
+import React, { useEffect, useState } from "react";
+import ProductList from "../../components/ProductList";
 import { Main, TituloH1 } from "./styles";
-
-const products: Product[] = [
-  {
-    id: 1,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 2,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 3,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 4,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 5,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 6,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 7,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 8,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 9,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 10,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 11,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 12,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 13,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 14,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 15,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 16,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 18,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 19,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 20,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 21,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 22,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 23,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 24,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 25,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 26,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 27,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 28,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 29,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 30,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 31,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 32,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-  {
-    id: 33,
-    name: "Produto 2",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 19.99,
-  },
-  {
-    id: 34,
-    name: "Produto 1",
-    photo: "../../../assets/images/logo-com-nome.png",
-    price: 10.99,
-  },
-
-  // Adicione mais produtos aqui
-];
+import { listarProdutos } from "../../services/MainApi/produtos";
 
 export default function Produtos() {
   const [open, setOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await listarProdutos();
+        setProducts(response.data.result || []);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      }
+    }
+
+    fetchProducts();
+  }, []);
 
   return (
     <Main>
@@ -221,3 +33,18 @@ export default function Produtos() {
     </Main>
   );
 }
+
+// return (
+//   <Wrapper>
+//     <StyledSlider {...settings}>
+//       {productImages.map((image: ProductImage) => (
+//         <div key={image.id}>
+//           <Link to={caminhoDaPagina + image.id}>
+//             <Img src={image.image} />
+//           </Link>
+//         </div>
+//       ))}
+//     </StyledSlider>
+//   </Wrapper>
+// );
+// }
