176c1887a0ff2b0ce2ab653d73efe7e8f6d9be2c
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-06-05 01:42:09 -0400

    Ajustes no carrinho

--- src/components/CartTable/index.tsx
+++ src/components/CartTable/index.tsx
@@ -1,78 +1,73 @@
-import React, { useEffect} from "react"
-// import { Container } from "./style";
+import React, { useEffect } from "react";
+import { Container } from "./style";
 import { listarCarrinho } from "../../services/MainApi/carrinho";
-// import { baseAPI } from "../../services/MainApi/carrinho";
-
-
-
-//MOCK PRODUCTS
+import { baseAPI } from "../../services/MainApi/carrinho";
 
 interface CartTableProps {
-    data: Produto;
-    handleRemoveItem: (props: Produto) => void;
+  data: Produto;
+  handleRemoveItem: (props: Produto) => void;
 }
 interface Produto {
-    _id: string
-    name: string
-    category: string
-    price: number
-    quantity: number
-  }
-const CartTable = (props:CartTableProps, handleRemoveItem:CartTableProps)=>{
-    
-    
-    // const [CarrinhoProdutos, setCarrinhoProdutos] = useState<CarrinhoProduto[]>([]);
-    // useEffect(()=>{
-    //     const getData =async () => {
-    //         try {
-    //             const response = await listarCarrinho();
-    //             setCarrinho(response.data)                
-    //         } catch (error) {
-    //             alert ('algo deu errado')                
-    //         }           
-    //     };
-    //     getData();
-    // },[setCarrinho])        
+  _id: string;
+  name: string;
+  category: string;
+  price: number;
+  quantity: number;
+}
+const CartTable = (props: CartTableProps, handleRemoveItem: CartTableProps) => {
+  const [CarrinhoProdutos, setCarrinhoProdutos] = useState<CarrinhoProduto[]>(
+    []
+  );
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await listarCarrinho();
+        setCarrinho(response.data);
+      } catch (error) {
+        alert("algo deu errado");
+      }
+    };
+    getData();
+  }, [setCarrinho]);
 
-    useEffect(()=>(console.log(props)))
-    return (        
-        <tr>
-            <td>
-                <div className="produto">
-                    <div className="info">
-                        <div className="nome">{props.data.name}</div>
-                    </div>
-                </div>
-            </td>
-                <td>{props.data.price}</td>
-                <td>
-                    <div className="quantidade">
-                        <button>
-                            <i> - </i>                        
-                         </button>
-                        <span>{props.data.quantity}</span>
-                        <button>
-                            <i> + </i>
-                     </button>
-                    </div>
-                </td>
-                <td>{(props.data.price)*(props.data.quantity)}</td>
-                <td>
-                    <button className="btn-remove-produto" onClick={()=>handleRemoveItem}>
-                        <i> remover </i>
-                    </button>
-                </td>            
-        </tr>
-        )
-    }
-    
-    export default CartTable
+  useEffect(() => console.log(props));
+  return (
+    <tr>
+      <td>
+        <div className="produto">
+          <div className="info">
+            <div className="nome">{props.data.name}</div>
+          </div>
+        </div>
+      </td>
+      <td>{props.data.price}</td>
+      <td>
+        <div className="quantidade">
+          <button>
+            <i> - </i>
+          </button>
+          <span>{props.data.quantity}</span>
+          <button>
+            <i> + </i>
+          </button>
+        </div>
+      </td>
+      <td>{props.data.price * props.data.quantity}</td>
+      <td>
+        <button className="btn-remove-produto" onClick={() => handleRemoveItem}>
+          <i> remover </i>
+        </button>
+      </td>
+    </tr>
+  );
+};
 
+export default CartTable;
 
-    // {cart.map(item=> <CartTable/>)}
-    //             {cart.length === 0 && (
-    //             <tr>
-    //               <td colSpan={5} style={{textAlign: 'center'}}>
-    //                 <b>Seu carrinho de compras está vazio.</b>
-    //               </td>
-    //             </tr> )}   
+// {cart.map(item=> <CartTable/>)}
+//             {cart.length === 0 && (
+//             <tr>
+//               <td colSpan={5} style={{textAlign: 'center'}}>
+//                 <b>Seu carrinho de compras está vazio.</b>
+//               </td>
+//             </tr> )}
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,104 +1,102 @@
 import { useEffect, useState } from "react";
 import { Footer } from "../../components/Footer";
 import Header from "../../components/Header";
-// import {baseAPI, listarCarrinho, atualizarCarrinho, cadastroCarrinho, deletarCarrinho} from "../../services/MainApi/carrinho"
+import {
+  baseAPI,
+  listarCarrinho,
+  atualizarCarrinho,
+  cadastroCarrinho,
+  deletarCarrinho,
+} from "../../services/MainApi/carrinho";
 
-import carrinho from "../../services/MainApi/config"
+import carrinho from "../../services/MainApi/config";
 import CartTable from "../../components/CartTable";
 import { Container } from "./style";
 
-interface Produto  {
-  _id: string
-  name: string
-  category: string
-  price: number
-  quantity: number
+interface Produto {
+  _id: string;
+  name: string;
+  category: string;
+  price: number;
+  quantity: number;
 }
 
-
-// todo X fazer um placeholder para quando não houver itens no carrinho
-//     - inserção de novos produtos no carrinho
-//     - remoção de produtos
-//     - alteração de quantidade
-//     - calculo de preço total
-//     - Puxar info das APIs
-//     - Checar se está logado
-//     - cupom de desconto?
-
-// MOCK ITEM CREATOR
-function randomNumber (min: number, max: number){
-  return Math.floor(Math.random()*(max-min)+min);
+function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 export default function Carrinho() {
   const [open, setOpen] = useState(false);
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-  
   const productObject = {
-    name: 'produto',
-    category: 'categoria',
-    price: randomNumber(80,1200),
-    quantity: 1
+    name: "produto",
+    category: "categoria",
+    price: randomNumber(80, 1200),
+    quantity: 1,
   };
-  useEffect(()=>{
-    // getData();
-  },[]);
-  
-  // const getData = ()=>{    
-  //   baseAPI.get('/cart').then((response)=> setCart(response.data))
-  // }
+  useEffect(() => {
+    getData();
+  }, []);
 
-  // const handleAddItem = ()=>{
-  //   baseAPI.post('/cart',productObject).then((response)=>{
-  //     getData();
-  //     console.log(response);
-  //     console.log('Disparou addItem');})
-  // }
+  const getData = () => {
+    baseAPI.get("/cart").then((response) => setCart(response.data));
+  };
 
-  const handleRemoveItem = (item: Produto)=>{    
-    console.log({item});
-    console.log('disparou remove item')
-    
-  }
-  const handleUpdateItem = ()=>{
-    return
-  }
- 
+  const handleAddItem = () => {
+    baseAPI.post("/cart", productObject).then((response) => {
+      getData();
+      console.log(response);
+      console.log("Disparou addItem");
+    });
+  };
+
+  const handleRemoveItem = (item: Produto) => {
+    console.log({ item });
+    console.log("disparou remove item");
+  };
+  const handleUpdateItem = () => {
+    return;
+  };
 
   return (
     <main>
-
       <Header open={open} setOpen={setOpen} />
 
       <main>
         <h1>Carrinho</h1>
         {/* <button onClick={handleAddItem} style={{padding: '5px 10px', margin: '15px' }}>add to cart</button> */}
         <div className="cart-content">
-          <Container>            
-              <thead>
-                <th>Produto</th>
-                <th>Preço</th>
-                <th>Quantidade</th>
-                <th>Total</th>
-                <th>-</th>
-              </thead>
-              <tbody>
+          <Container>
+            <thead>
+              <th>Produto</th>
+              <th>Preço</th>
+              <th>Quantidade</th>
+              <th>Total</th>
+              <th>-</th>
+            </thead>
+            <tbody>
               {cart.map((props: Produto) => (
-  <CartTable data={props} key={props._id} handleRemoveItem={()=>handleRemoveItem(props)} />
-))};
-                {cart.length===0 && (<tr>
-                  <td colSpan={5} style={{textAlign:'center'}}>
+                <CartTable
+                  data={props}
+                  key={props._id}
+                  handleRemoveItem={() => handleRemoveItem(props)}
+                />
+              ))}
+              ;
+              {cart.length === 0 && (
+                <tr>
+                  <td colSpan={5} style={{ textAlign: "center" }}>
                     <b>Carrinho de compras vazio.</b>
                   </td>
-                </tr>)}
-              </tbody>            
-          </Container>         
-        </div>        
+                </tr>
+              )}
+            </tbody>
+          </Container>
+        </div>
       </main>
-
 
       <Footer />
     </main>
-  )
+  );
 }
