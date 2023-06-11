b8b0c2fdf2f071ebe278fdbe1fd40f901b77e79e
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-06-11 13:40:36 -0400

    Otimização no componente ProductSearch

--- src/components/ProductSearch/index.tsx
+++ src/components/ProductSearch/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   InputDefault,
   ResultadosPesquisa,
@@ -22,21 +23,24 @@ export type Produto = {
   image: string;
 };
 
-export default function ProductSearch(props: Props) {
+export default function ProductSearch({ value, onChange }: Props) {
   const caminhoDaPagina = "/produto/";
 
-  const { data, isLoading } = useQuery(
-    ["pesquisaDeProduto", props.value],
-    () => findProduct(props.value),
-    { enabled: props.value.length > 2 }
+  // Utiliza o hook useQuery do react-query para realizar a busca dos produtos
+  const { data, isLoading, isError } = useQuery(
+    ["pesquisaDeProduto", value], // Define uma chave única para a query baseada no valor da pesquisa
+    () => findProduct(value), // Função que realiza a busca dos produtos
+    {
+      enabled: value.length > 2, // Define se a query deve ser executada com base no tamanho do valor da pesquisa
+    }
   );
 
   async function findProduct(query: string): Promise<Produto[]> {
     try {
-      const response = await listarProdutos();
+      const response = await listarProdutos(); // Chama a função listarProdutos para obter os produtos
       const produtos = response.data.result;
 
-      // Filtrar os produtos que correspondem à pesquisa
+      // Filtra os produtos que correspondem à pesquisa com base no título
       const produtosFiltrados = produtos.filter((produto: Produto) =>
         produto.title.toLowerCase().includes(query.toLowerCase())
       );
@@ -44,28 +48,28 @@ export default function ProductSearch(props: Props) {
       return produtosFiltrados;
     } catch (error) {
       console.error("Erro ao buscar produtos:", error);
-      return [];
+      throw new Error("Falha ao buscar produtos.");
     }
   }
 
   return (
     <SearchComponent>
       <InputDefault
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         type="text"
         name="produto"
         placeholder="Busque produtos"
       />
       {isLoading && <LoadingText>Carregando ...</LoadingText>}
+      {isError && <div>Erro ao buscar produtos.</div>}
       {data && data.length > 0 && (
         <ResultadosPesquisa>
-          {data?.map((produto) => (
+          {/* Renderiza os resultados da pesquisa como links para os produtos */}
+          {data.map((produto) => (
             <div className="row" key={produto.id}>
               <div className="column">
-                <Link to={caminhoDaPagina + produto.id}>
-                  {produto.title}
-                </Link>
+                <Link to={caminhoDaPagina + produto.id}>{produto.title}</Link>
               </div>
             </div>
           ))}
