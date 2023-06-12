4e6ed8eaed39a1eaff9ffbc9a0daa0976132b520
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-06-11 22:50:48 -0400

    Feature: ajustes na pagina de carrinho e na pagina minha conta

--- src/modules/MinhaConta/PagMinhaConta/index.tsx
+++ src/modules/MinhaConta/PagMinhaConta/index.tsx
@@ -1,114 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Footer } from "../../../components/Footer";
 import Header from "../../../components/Header";
-import {Container, ListItem, LogoutBtn} from "./styles";
+import { Container, ListItem, LogoutBtn } from "./styles";
 import { useNavigate } from "react-router-dom";
+import { listarPedidos, PedidoPayload } from "../../../services/MainApi/pedidos";
 
 export default function MinhaConta() {
   const [open, setOpen] = useState(false);
-
-
-  const pedidos = [
-		{
-			idCompra: 1,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Aguardando pagamento',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 2,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 3,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Em curso',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-    },
-    {
-			idCompra: 4,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 5,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 6,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 7,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 8,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		},
-    {
-			idCompra: 9,
-			cliente: 'Jonathas Pereira Golçalves',
-			situacao: 'Entregue',
-			data: '15/04/2023',
-      valor: 'R$ 45,00',
-		}
-	] 
+  const [pedidos, setPedidos] = useState<PedidoPayload[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPedidos = async () => {
+      try {
+        const response = await listarPedidos();
+        const { success, result } = response.data;
+
+        if (success) {
+          setPedidos(result);
+        } else {
+          setError(() => "Ocorreu um erro ao obter os pedidos. Por favor, tente novamente mais tarde.");
+        }
+      } catch (error) {
+        console.error("Erro ao obter pedidos:", error);
+        setError(() => "Ocorreu um erro ao obter os pedidos. Por favor, tente novamente mais tarde.");
+      }
+    };
+
+    fetchPedidos();
+  }, []);
 
   const logout = () => {
     localStorage.removeItem("token"); // Remove o token antigo, se existir
     navigate("/"); // Redirecionamento para a página Home
   };
-  
+
   return (
     <main>
       <Header open={open} setOpen={setOpen} />
 
-      <LogoutBtn onClick={() => logout()}>Logout</LogoutBtn>
+      <LogoutBtn onClick={logout}>Logout</LogoutBtn>
 
       <Container>
         <thead>
-          <th>Código da compra</th>
-          <th>Cliente</th>
-          <th>Situação do pedido</th>
-          <th>Data da compra</th>
-          <th>Valor</th>
+          <tr>
+            <th>Código da compra</th>
+            <th>Cliente</th>
+            <th>Situação do pedido</th>
+            <th>Data da compra</th>
+            <th>Valor</th>
+          </tr>
         </thead>
 
         <tbody>
-        <>{pedidos.map((item) => (
-          <tr>
-            <ListItem>{item.idCompra}</ListItem>
-            <ListItem>{item.cliente}</ListItem>
-            <ListItem>{item.situacao}</ListItem>
-            <ListItem>{item.data}</ListItem>
-            <ListItem>{item.valor}</ListItem>
-          </tr>
-        ))}</> 
+          {pedidos.map((order) => (
+            <tr key={order.id}>
+              <ListItem>{order.id}</ListItem>
+              <ListItem>{order.buyer}</ListItem>
+              <ListItem>{order.order_status}</ListItem>
+              <ListItem>{order.created_at}</ListItem>
+              <ListItem>{order.total_value}</ListItem>
+            </tr>
+          ))}
         </tbody>
       </Container>
-      
+
+      {error && <p className="error-message">{error}</p>}
+
       <Footer />
     </main>
   );
